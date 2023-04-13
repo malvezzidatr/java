84a90d3ae65b8fb53af507093b903cc009e85f6b
Caio Malvezzi <caiomalvezzi101@gmail.com>
2023-04-13 16:16:38 -0300

    Basico sobre classes

---
+++ classe/src/classe/ProdutoTeste.java
@@ -0,0 +1,28 @@
+package classe;
+
+import java.util.Scanner;
+
+public class ProdutoTeste {
+	public static void main(String[] args) {
+		
+		Produto p1 = new Produto();
+		p1.nome = "Notebook";
+		p1.preco = 4356.89;
+		p1.desconto = 0.25;
+		
+		Produto p2 = new Produto();
+		p2.nome = "Caneta";
+		p2.preco = 12.56;
+		p2.desconto = 0.29;
+		
+		Double p1PrecoFinal = p1.preco * (1 - p1.desconto);
+		Double p2PrecoFinal = p2.preco * (1 - p1.desconto);
+		Double mediaCarrinho = (p1PrecoFinal + p2PrecoFinal) / 2;
+		
+		System.out.println(p1.nome);
+		System.out.println(p1PrecoFinal);
+		System.out.println(p2.nome);
+		System.out.println(p2PrecoFinal);
+		System.out.printf("Media do carrinho = R$%.2f.", mediaCarrinho);
+	}
+}
